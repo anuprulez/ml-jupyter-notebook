778edbb5138fe69fc84ba58655995b6aa86b2a52
anup kumar <kumara@informatik.uni-freiburg.de>
2021-02-17 19:00:47 +0100

    Update startup script

--- startup.sh
+++ startup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#export LD_LIBRARY_PATH=/usr/local/cuda-11.0/lib64:$LD_LIBRARY_PATH
+#export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+
 if [ ! -f /import/tensorflow_notebook.ipynb ]; then
     cp /home/$NB_USER/tensorflow_notebook.ipynb /import/tensorflow_notebook.ipynb
     chown $NB_USER /import/tensorflow_notebook.ipynb
