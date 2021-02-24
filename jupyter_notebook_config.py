883e1dcc83124369c936aa11539d99f4aa13b19e
Anup Kumar <anup.rulez@gmail.com>
2021-02-24 11:33:37 +0000

    Update URL

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -32,9 +32,9 @@ headers = {
 c.ServerApp.allow_origin = '*'
 c.ServerApp.allow_credentials = True
 
-c.NotebookApp.base_url = '/ipython/' #'%s/ipython/' % os.environ.get('PROXY_PREFIX', '')
+c.NotebookApp.base_url = '%s/ipython/' % os.environ.get('PROXY_PREFIX', '')
 c.NotebookApp.tornado_settings = {
-    'static_url_prefix': '/ipython/static/' #'%s/ipython/static/' % os.environ.get('PROXY_PREFIX', '')
+    'static_url_prefix': '%s/ipython/static/' % os.environ.get('PROXY_PREFIX', '')
 }
 
 if os.environ.get('NOTEBOOK_PASSWORD', 'none') != 'none':
