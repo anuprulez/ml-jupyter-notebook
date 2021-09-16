c1c30d60370e8e43fa461fbd91d56c537ef45bf4
Anup Kumar <anup.rulez@gmail.com>
2021-09-16 17:08:05 +0200

    add check job status

--- get_dynamic_code.py
+++ get_dynamic_code.py
@@ -13,6 +13,7 @@ def run_dynamic_code(script_path, server, key, new_history_name="ml_analysis", t
     #new_history = gi.histories.create_history(new_history_name)
     gi = GalaxyInstance(server, key=key)
     history = histories.HistoryClient(gi)
+    job_client = jobs.JobsClient(gi)
     new_history = history.create_history(new_history_name)
     # get script
     from nbformat import read, NO_CONVERT
@@ -27,9 +28,14 @@ def run_dynamic_code(script_path, server, key, new_history_name="ml_analysis", t
     with open(target_file_name, "w") as f_obj:
         f_obj.write(notebook_script)
     # upload script
-    uploaded_dataset = gi.tools.upload_file(target_file_name, new_history["id"])
-    sleep(40)
+    upload_job = gi.tools.upload_file(target_file_name, new_history["id"])
+    upload_job_id = upload_job["jobs"][0]["id"]
+    upload_job_status = job_client.get_state(upload_job_id)
+    while upload_job_status in ["new", "queued", "running", "waiting"]:
+        upload_job_status = job_client.get_state(upload_job_id)
+        print(upload_job_status)
+        sleep(5)
     hist_id = new_history["id"]
-    uploaded_file_path = uploaded_dataset["outputs"][0]["id"]
-    tool_run = gi.tools.run_tool(hist_id, tool_name, {"inputs": {"select_file": uploaded_file_path}})
-    return tool_run
+    upload_file_path = upload_job["outputs"][0]["id"]
+    code_execute_job = gi.tools.run_tool(hist_id, tool_name, {"inputs": {"select_file": upload_file_path}})
+    return job_client, upload_job, code_execute_job
